f8c99bcf7907fb45711a8d55090b5eb50bb51521
leenbean16 <shamoonlina@gmail.com>
2017-06-07 19:42:29 -0700

    final changes

--- winner.js
+++ winner.js
@@ -2,10 +2,9 @@ var btn = $('.start');
 $('#winner-animation').hide();
 btn.on('click', function(){
 	$('#winner-animation').show();
+setTimeout(function(){ 
+	$('#winner-animation').fadeOut("slow");
+}, 3000);
 })
 
 
-setTimeout(function(){ 
-	$('#winner-animation').hide();
-}, 
-4000);
